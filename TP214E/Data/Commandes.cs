843dd0cdfbf9e68eb01bd32e4b2c094f6204f7bd
Willyam <willyam.arcand@gmail.com>
2021-11-04 16:28:59 -0400

    Classe: commande

--- TP214E/Data/Commandes.cs
+++ TP214E/Data/Commandes.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MongoDB.Bson;
 
 namespace TP214E.Data
 {
     public class Commandes
     {
+        public ObjectId id { get; set; }
+        public List<Plat> Plats { get; set; }
+        public DateTime date { get; set; }
+
+        public float SousTotal
+        {
+            get
+            {
+                float sommeDesPrix = 0;
+                Plats.ForEach(plat => sommeDesPrix+=plat.Prix);
+                return sommeDesPrix;
+            }
+        }
+
+        public float Taxe
+        {
+            get { return SousTotal*(float)0.15; }
+        }
+
+        public float Total
+        {
+            get { return SousTotal+Taxe; }
+        }
     }
 }
