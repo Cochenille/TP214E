3d4d0d787596b1277e77a80615501757e3eb57ea
Willyam <willyam.arcand@gmail.com>
2021-11-04 16:24:38 -0400

    Classe: plat

---
+++ TP214E/Data/Plat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+
+namespace TP214E.Data
+{
+    public class Plat
+    {
+        public ObjectId Id { get; set; }
+        public string Nom { get; set; }
+        public List<Aliment> Aliments { get; set; }
+        public float Prix { get; set; }
+
+    }
+}
